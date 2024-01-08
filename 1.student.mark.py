be1288e4189cccf4fcaa12d9102baa72973397eb
IcyBoy1202 <anhducvn2003@gmail.com>
2024-01-08 10:34:12 +0700

    Test upload to GH

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,21 +1,66 @@
-def student_call():
-    s_name = []
-    s_id = []
-    s_DoB = []
-    num_of_student = int(input())
+'''def student_info(s):
+    s.name = []
+    s.id = []
+    s.DoB = []
+    num_of_student = int(input("Number of students: "))
     for i in range(num_of_student):
-       s_name += [str(input("Your name here: "))]
-       s_id += [str(input("Your student id here:"))]
-       s_DoB += [str(input("Your DoB here:"))]
-    print(s_name, s_id, s_DoB)
+       s.name += [str(input("Student name here: "))]
+       s.id += [str(input("Student id here: "))]
+       s.DoB += [str(input("Student DoB here: "))]
+    print(s.name, s.id, s.DoB)
 
-def course_call():
-     c_name = []
-     c_id = []
-     num_of_courses = int(input())
+def course_info(c):
+     c.name = []
+     c.id = []
+     num_of_courses = int(input("Number of courses: "))
      for i in range(num_of_courses):
-       c_name += [str(input("Your course name here:" ))]
-       c_id += [str(input("Your course id here:"))]
-     print(c_name, c_id)
-student_call()
-course_call()
+       c.name += [str(input("Course name here: "))]
+       c.id += [str(input("Course id here: "))]
+     print(c.name, c.id)
+
+
+student_info()
+course_info() '''
+
+class Student:
+    def __init__(self):
+        self.sname = []
+        self.sid = []
+        self.sDoB = []
+
+class Course:
+    def __init__(self):
+        self.cname = []
+        self.cid = []
+
+def student_info(s):
+    num_of_student = int(input("Number of students: "))
+    for i in range(num_of_student):
+        s.sname.append(str(input("Student name here: ")))
+        s.sid.append(str(input("Student id here: ")))
+        s.sDoB.append(str(input("Student DoB here: ")))
+    print(s.sname, s.sid, s.sDoB)
+
+def course_info(c):
+    num_of_courses = int(input("Number of courses: "))
+    for i in range(num_of_courses):
+        c.cname.append(str(input("Course name here: ")))
+        c.cid.append(str(input("Course id here: ")))
+    print(c.cname, c.cid)
+
+def student_call(self):
+    search_student = input("Student you want to call: ")
+    if search_student := self.sname:
+      print("success")
+    else:
+      print("failure")
+
+# Create instances of Student and Course classes
+student_data = Student()
+course_data = Course()
+
+
+# Call functions with instances as arguments
+student_info(student_data)
+course_info(course_data)
+student_call(student_data)
