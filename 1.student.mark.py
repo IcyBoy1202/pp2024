6be52d83fc2a820d865fbccb4ab27726e100b24c
IcyBoy1202 <anhducvn2003@gmail.com>
2024-01-08 10:03:02 +0700

    Test upload to GitHub, not the finish version

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -4,9 +4,9 @@ def student_call():
     s_DoB = []
     num_of_student = int(input())
     for i in range(num_of_student):
-       s_name += [str(input())]
-       s_id += [str(input())]
-       s_DoB += [str(input())]
+       s_name += [str(input("Your name here: "))]
+       s_id += [str(input("Your student id here:"))]
+       s_DoB += [str(input("Your DoB here:"))]
     print(s_name, s_id, s_DoB)
 
 def course_call():
@@ -14,8 +14,8 @@ def course_call():
      c_id = []
      num_of_courses = int(input())
      for i in range(num_of_courses):
-       c_name += [str(input())]
-       c_id += [str(input())]
+       c_name += [str(input("Your course name here:" ))]
+       c_id += [str(input("Your course id here:"))]
      print(c_name, c_id)
 student_call()
 course_call()
